b7618b45afaea88a0e7c4d4f3737c6ce651807e6
Joshua Oren <joshuadavidoren@gmail.com>
2021-07-15 16:45:57 -0500

    created form and useState for it

--- src/components/screenTwo/screenTwo.jsx
+++ src/components/screenTwo/screenTwo.jsx
@@ -1,2 +1,41 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import './screenTwo.css';
-$
+
+function screenTwo() {
+    const [nameInput, setNameInput] = useState('');
+    const [streetAddressInput, setStreetAddressInput] = useState('');
+    const [cityInput, setCityInput] = useState('');
+    const [zipInput, setZipInput] = useState('');
+
+    // create dispatch for redux state of customer info
+    const dispatch = useDispatch();
+
+
+
+// create html form for customer info
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                placeholder='Name'
+                value={nameInput}
+                pnChange={(evt) => setNameInput(evt.target.value)}
+            />
+            <input
+                placeholder='Street Address'
+                value={streetAddressInput}
+                pnChange={(evt) => setStreetAddressInput(evt.target.value)}
+            />
+            <input
+                placeholder='City'
+                value={cityInput}
+                pnChange={(evt) => setCityInput(evt.target.value)}
+            />
+            <input
+                placeholder='Zip'
+                value={zipInput}
+                pnChange={(evt) => setZipInput(evt.target.value)}
+            />
+        </form>
+    )
+}
