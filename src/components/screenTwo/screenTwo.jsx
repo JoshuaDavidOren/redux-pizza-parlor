17e3059c4536f13182c38e9a6cdefc0821c7e7b5
Joshua Oren <joshuadavidoren@gmail.com>
2021-07-15 17:16:27 -0500

    html form built out with useState imported into app.jsx

--- src/components/screenTwo/screenTwo.jsx
+++ src/components/screenTwo/screenTwo.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import './screenTwo.css';
 
-function screenTwo() {
+function ScreenTwo() {
     const [nameInput, setNameInput] = useState('');
     const [streetAddressInput, setStreetAddressInput] = useState('');
     const [cityInput, setCityInput] = useState('');
@@ -16,35 +16,45 @@ function screenTwo() {
 
 // create html form for customer info
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                placeholder='Name'
-                value={nameInput}
-                onChange={(evt) => setNameInput(evt.target.value)}
-            />
-            <input
-                placeholder='Street Address'
-                value={streetAddressInput}
-                onChange={(evt) => setStreetAddressInput(evt.target.value)}
-            />
-            <input
-                placeholder='City'
-                value={cityInput}
-                onChange={(evt) => setCityInput(evt.target.value)}
-            />
-            <input
-                placeholder='Zip'
-                value={zipInput}
-                onChange={(evt) => setZipInput(evt.target.value)}
-            />
-            <input 
-            type='checkbox' 
-            id='delivery' 
-            value='true'
-            onChange={(evt) => setDeliverInput(evt.target.value)}/>
-                <label>Check for Delivery</label>
-            <button type='submit' value='submit'>NEXT</button> 
+        <section>
+            <div>
+                <label>Total:
+                     {/* priceOfOrder */}
+                </label>
+            </div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    placeholder='Name'
+                    value={nameInput}
+                    onChange={(evt) => setNameInput(evt.target.value)}
+                />
+                <input
+                    placeholder='Street Address'
+                    value={streetAddressInput}
+                    onChange={(evt) => setStreetAddressInput(evt.target.value)}
+                />
+                <input
+                    placeholder='City'
+                    value={cityInput}
+                    onChange={(evt) => setCityInput(evt.target.value)}
+                />
+                <input
+                    placeholder='Zip'
+                    value={zipInput}
+                    onChange={(evt) => setZipInput(evt.target.value)}
+                />
+                <input
+                type='checkbox'
+                id='delivery'
+                value={!deliveryInput}
+                onChange={(evt) => setDeliverInput(evt.target.value)}/>
+                    <label>Check for Delivery</label>
+                <button type='submit' value='submit'>NEXT</button>
+            </form>
+        </section>
 
-        </form>
+
     )
 }
+
+export default ScreenTwo;
