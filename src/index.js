6139a46361e9383ee96cd992a2de3ed46eaf29c3
Steve Savstrom <steve.savstrom@gmail.com>
2021-07-15 15:23:24 -0500

    set up index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App/App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Imports - Added by Steve
+import { applyMiddleware, createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+
+// Reducers go here
+
+// Store goes here  - Added by Steve
+const storeInstance = createStore(
+  combineReducers({
+    //  Reducers go here
+  }),
+  applyMiddleware(logger)
+);
+
+// Provider added to ReactDOM - Added by Steve
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={storeInstance}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
