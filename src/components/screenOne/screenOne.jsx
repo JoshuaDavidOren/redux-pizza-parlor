ac7342c446fcee5e77dcd264681c862d88858212
Meghan Cleland <mplscleland@gmail.com>
2021-07-15 21:53:57 -0500

    updated screenAdmin

--- src/components/screenOne/screenOne.jsx
+++ src/components/screenOne/screenOne.jsx
@@ -1,2 +1,70 @@
-import './screenOne.css';
-$
+import "./screenOne.css";
+import { useSelector } from "react-redux";
+
+function ScreenOne() {
+  const pizzaList = useSelector((store) => store.pizzaList);
+//   const pizzaSelected = [newPizzaOrder, setNewPizzaOrder]
+// 
+
+  return (
+	<div>
+	<table>
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Description</th>
+				<th>Price</th>
+				<th></th>
+			</tr>
+		</thead>
+		<tbody>
+			{pizzaList.map((pizza, index) => (
+				<tr key={index}>
+					<td>
+						{pizza.name}
+					</td>
+					<td>
+						{pizza.description}
+					</td>
+					<td>
+						{pizza.price}
+					</td>
+					<td>
+						<button>Add</button>
+					</td>
+				</tr>
+			))}
+		</tbody>
+	</table>
+
+	<button>Next</button>
+</div>
+  );
+}
+
+export default ScreenOne;
+
+{/* <section>
+<h2>Step 1: Select Your Pizza</h2>
+<section>
+  {pizzaList.map((pizza, index) => (
+	<p id="menuCard" key={index}> {pizza.name} </p>
+  ))}
+</section>
+</section> */}
+
+{
+  /* <table>
+<tbody>
+	<tr>
+		<td>Name</td>
+		<td>Description</td>
+		<td>Price</td>
+		<td>Image</td>
+		<td> 
+			<button>Add</button>
+		</td>
+	  </tr>
+  </tbody>
+</table> */
+}
